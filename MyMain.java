5c828a1ce13281e5ebc7d366a85080746e1a753b
milesrtaylor <miles.taylor@student.nmt.edu>
2025-02-21 10:00:10 -0700

    general structure of main
    Still need to add student and transaction functionality

--- MyMain.java
+++ MyMain.java
@@ -9,6 +9,7 @@ import java.util.Scanner;
 public class MyMain {
 
 	public static void main(String[] args) {
+		// TODO might want to put all file i/o in another class, also need to go thru students and transactions
 		File book_list = new File("Books_List.csv");
 		File student_list = new File("Students_List.csv");
 		File transaction_list = new File("Borrow_Records.csv");
@@ -16,26 +17,82 @@ public class MyMain {
 		List<Book> books = new ArrayList<>();
 		try (Scanner scanner = new Scanner(book_list)) {
             String line;
+            // TODO need to skip first line
             while ((line = scanner.nextLine()) != null) {
                 String[] data = line.split(",");
                 books.add(new Book(Integer.parseInt(data[0]), data[1],
                 		data[2], data[3], data[4], Boolean.parseBoolean(data[5])));
             }
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
+			System.out.println("Error: File not found.");
 			e.printStackTrace();
 		}
 		
 		while (true) {
-			Scanner s1;
+			Scanner input = new Scanner(System.in);
             System.out.println("Library System:");
             System.out.println("1. Search Book");
             System.out.println("2. Borrow Book");
             System.out.println("3. Return Book");
             System.out.println("4. View All Books");
-            System.out.println("5. Exit");
+            System.out.println("5. Enter Librarian Password");
+            System.out.println("6. Exit");
             System.out.print("Choose an option: ");
-            int choice = s1.nextInt();
-            s1.nextLine(); 
+            int choice = input.nextInt();
+            input.nextLine(); 
+            
+            switch (choice) {
+                case 1: 
+                	System.out.print("Enter title, author, or category: ");
+        	        String query = input.nextLine().toLowerCase();
+        	        for (Book book : books) { // might be better to have this as a function in Book.java, likewise with the other cases
+        	            if (book.getTitle().toLowerCase().contains(query)
+        	            		|| book.getAuthor().toLowerCase().contains(query)
+        	            		|| book.getCategory().toLowerCase().contains(query))
+        	                System.out.println(book);
+        	        }
+                	break;
+                case 2:
+                	System.out.print("Enter Book ID to borrow: ");
+        	        int borrow_id = input.nextInt();
+        	        for (Book book : books) {
+        	            if (book.getId() == borrow_id && book.isAvailable()) {
+        	                book.setAvailability(false);
+        	                System.out.println("Book borrowed successfully.");
+        	                break; // test
+        	            }
+        	        }
+        	        System.out.println("Book unavailable.");
+                	break;
+                case 3: 
+                	System.out.print("Enter Book ID to return: ");
+        	        int return_id = input.nextInt();
+        	        for (Book book : books) {
+        	            if (book.getId() == return_id && !book.isAvailable()) {
+        	                book.setAvailability(true);
+        	                System.out.println("Book returned successfully.");
+        	                break; // test
+        	            }
+        	        }
+        	        System.out.println("Invalid book ID.");
+                	break;
+                case 4: 
+                	for (Book book : books) {
+        	            System.out.println(book);
+        	        }
+                	break;
+                case 5:
+                	String password = input.nextLine();
+                	Librarian x = new Librarian(password); // TODO: need incorrect password case too
+                	x.showMenu(); // move to main?
+                	break;
+                case 6: 
+                	System.out.println("Exiting..."); 
+                default: 
+                	System.out.println("Invalid choice!");
+            }
+            
+            input.close();
 		}
 	}
+}
