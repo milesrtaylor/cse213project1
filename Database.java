b0245e1ce199c34aacac6a29049b929cf77f6fe7
milesrtaylor <miles.taylor@student.nmt.edu>
2025-02-21 16:22:44 -0700

    added Database
    handles file i/o

---
+++ Database.java
@@ -0,0 +1,62 @@
+package project1;
+
+import java.io.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Database {
+	private static String book_file = "Books_List.csv";
+    private static String student_file = "Students_List.csv";
+    private static String transaction_file = "Borrow_Records.csv";
+    
+    public static List<Book> loadBooks() throws IOException {
+        List<Book> books = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(book_file))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(",");
+                books.add(new Book(Integer.parseInt(data[0]), data[1], data[2],
+                		data[3], data[4], Boolean.parseBoolean(data[5])));
+            }
+        }
+        return books;
+    }
+    
+    public static List<Student> loadStudents() throws IOException {
+    	List<Student> students = new ArrayList<>();
+    	try (BufferedReader br = new BufferedReader(new FileReader(student_file))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(",");
+                students.add(new Student(Integer.parseInt(data[0]), data[1],
+                		Integer.parseInt(data[2]), data[3]));
+            }
+        }
+        return students;
+    }
+    
+    public static List<Transaction> loadTransactions() throws IOException {
+    	List<Transaction> transactions = new ArrayList<>();
+    	try (BufferedReader br = new BufferedReader(new FileReader(transaction_file))) {
+    		String line;
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(",");
+                transactions.add(new Transaction(Integer.parseInt(data[0]),
+                		Integer.parseInt(data[1]), Integer.parseInt(data[2]),
+                		LocalDate.parse(data[3]), LocalDate.parse(data[4]),
+                		LocalDate.parse(data[5])));
+            }
+    	}
+    	return transactions;
+    }
+
+    public static void saveBooks(List<Book> books) throws IOException {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(book_file))) {
+            for (Book book : books) {
+                bw.write(book.toString());
+                bw.newLine();
+            }
+        }
+    }
+}
