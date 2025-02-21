cc60559201abf3b790978456444509e98fa9d804
milesrtaylor <miles.taylor@student.nmt.edu>
2025-02-20 18:40:23 -0700

    added librarian functions [not done]
    Need to figure out how to get the book list and transaction list to the class but all other logic should be fine.

--- Librarian.java
+++ Librarian.java
@@ -1,5 +1,79 @@
 package project1;
 
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Scanner;
+
 public class Librarian {
 	private String password;
+	private List<Book> books;
+    private List<Transaction> transactions;
+    private Scanner scanner;
+    
+    public Librarian(String password) {
+        this.password = password;
+        this.books = ??; // TODO
+        this.transactions = ??; // TODO
+        this.scanner = new Scanner(System.in);
+    }
+
+    public boolean authenticate() {
+        System.out.print("Enter librarian password: ");
+        String inputPassword = scanner.nextLine();
+        return inputPassword.equals(this.password);
+    }
+
+    public void showMenu() {
+        if (!authenticate()) {
+            System.out.println("Incorrect password! Access denied.");
+            return;
+        }
+        
+        while (true) {
+            System.out.println("\nLibrarian Menu:");
+            System.out.println("1. List all books");
+            System.out.println("2. List borrowed books");
+            System.out.println("3. List overdue books");
+            System.out.println("4. Exit");
+            System.out.print("Choose an option: ");
+            
+            int choice = scanner.nextInt();
+            scanner.nextLine(); 
+
+            switch (choice) {
+                case 1: listAllBooks(); break;
+                case 2: listBorrowedBooks(); break;
+                case 3: listOverdueBooks(); break;
+                case 4: System.out.println("Exiting librarian menu..."); return;
+                default: System.out.println("Invalid choice! Try again.");
+            }
+        }
+    }
+
+    private void listAllBooks() {
+        System.out.println("\nAll Books:");
+        for (Book book : books) {
+            System.out.println(book);
+        }
+    }
+
+    private void listBorrowedBooks() {
+        System.out.println("\nBorrowed Books:");
+        for (Transaction transaction : transactions) {
+            if (transaction.getReturnDate() == null) { // Book not returned
+                System.out.println("Book ID: " + transaction.getBookId() + ", Borrowed by Student ID: " + transaction.getStudentId());
+            }
+        }
+    }
+
+    private void listOverdueBooks() {
+        System.out.println("\nOverdue Books:");
+        LocalDate today = LocalDate.now();
+        for (Transaction transaction : transactions) {
+            if (transaction.getReturnDate() == null && today.isAfter(transaction.getDueDate())) {
+                System.out.println("Book ID: " + transaction.getBookId() + " is overdue. Borrowed by Student ID: " + transaction.getStudentId());
+            }
+        }
+    }
 }
