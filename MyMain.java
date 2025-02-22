64cbe1ed8ed698621c7442b86683122df87bc2be
milesrtaylor <miles.taylor@student.nmt.edu>
2025-02-21 17:00:23 -0700

    update
    still need to fully implement student and librarian access

--- MyMain.java
+++ MyMain.java
@@ -9,87 +9,104 @@ import java.util.Scanner;
 public class MyMain {
 
 	public static void main(String[] args) {
-		// TODO might want to put all file i/o in another class, also need to go thru students and transactions
-		File book_list = new File("Books_List.csv");
-		File student_list = new File("Students_List.csv");
-		File transaction_list = new File("Borrow_Records.csv");
-		
-		List<Book> books = new ArrayList<>();
-		try (Scanner scanner = new Scanner(book_list)) {
-            String line;
-            // TODO need to skip first line
-            while ((line = scanner.nextLine()) != null) {
-                String[] data = line.split(",");
-                books.add(new Book(Integer.parseInt(data[0]), data[1],
-                		data[2], data[3], data[4], Boolean.parseBoolean(data[5])));
-            }
-		} catch (FileNotFoundException e) {
-			System.out.println("Error: File not found.");
-			e.printStackTrace();
-		}
-		
 		while (true) {
 			Scanner input = new Scanner(System.in);
-            System.out.println("Library System:");
-            System.out.println("1. Search Book");
-            System.out.println("2. Borrow Book");
-            System.out.println("3. Return Book");
-            System.out.println("4. View All Books");
-            System.out.println("5. Enter Librarian Password");
-            System.out.println("6. Exit");
+			List<Book> books = Database.loadBooks();
+			List<Student> students = Database.loadStudents();
+			List<Transaction> transactions = Database.loadTransactions();
+			
+			System.out.println("Welcome to the Socorro High School Library System");
+            System.out.println("1. Student Mode");
+            System.out.println("2. Librarian Mode");
             System.out.print("Choose an option: ");
             int choice = input.nextInt();
-            input.nextLine(); 
-            
+            input.nextLine();
             switch (choice) {
-                case 1: 
-                	System.out.print("Enter title, author, or category: ");
-        	        String query = input.nextLine().toLowerCase();
-        	        for (Book book : books) { // might be better to have this as a function in Book.java, likewise with the other cases
-        	            if (book.getTitle().toLowerCase().contains(query)
-        	            		|| book.getAuthor().toLowerCase().contains(query)
-        	            		|| book.getCategory().toLowerCase().contains(query))
-        	                System.out.println(book);
-        	        }
-                	break;
-                case 2:
-                	System.out.print("Enter Book ID to borrow: ");
-        	        int borrow_id = input.nextInt();
-        	        for (Book book : books) {
-        	            if (book.getId() == borrow_id && book.isAvailable()) {
-        	                book.setAvailability(false);
-        	                System.out.println("Book borrowed successfully.");
-        	                break; // test
-        	            }
-        	        }
-        	        System.out.println("Book unavailable.");
-                	break;
-                case 3: 
-                	System.out.print("Enter Book ID to return: ");
-        	        int return_id = input.nextInt();
-        	        for (Book book : books) {
-        	            if (book.getId() == return_id && !book.isAvailable()) {
-        	                book.setAvailability(true);
-        	                System.out.println("Book returned successfully.");
-        	                break; // test
-        	            }
-        	        }
-        	        System.out.println("Invalid book ID.");
-                	break;
-                case 4: 
-                	for (Book book : books) {
-        	            System.out.println(book);
-        	        }
-                	break;
-                case 5:
-                	String password = input.nextLine();
-                	Librarian x = new Librarian(password); // TODO: need incorrect password case too
-                	x.showMenu(); // move to main?
-                	break;
-                case 6: 
-                	System.out.println("Exiting..."); 
-                default: 
-                	System.out.println("Invalid choice!");
+            	case 1:
+	                System.out.print("Enter your Student ID: ");
+	                int student_id = input.nextInt();
+	                input.nextLine();
+	                
+	                boolean valid_id = false;
+	                for (Student student : students) {
+	                	if (student.getId() == student_id) {
+	                			valid_id = true;
+	                			// save student
+	                	}
+	                }
+	                if (valid_id) {
+	                    // everything else...
+	                	System.out.println("Student Library System:");
+	                    System.out.println("1. Search Book");
+	                    System.out.println("2. Borrow Book");
+	                    System.out.println("3. Return Book");
+	                    System.out.println("4. View All Books");
+	                    System.out.println("5. Enter Librarian Password");
+	                    System.out.println("6. Exit");
+	                    System.out.print("Choose an option: ");
+	                    int student_choice = input.nextInt();
+	                    input.nextLine(); 
+	                    
+	                    switch (student_choice) {
+	                        case 1: 
+	                        	System.out.print("Enter title, author, or category: ");
+	                	        String query = input.nextLine().toLowerCase();
+	                	        for (Book book : Database.loadBooks()) { // might be better to have this as a function in Book.java, likewise with the other cases
+	                	            if (book.getTitle().toLowerCase().contains(query)
+	                	            		|| book.getAuthor().toLowerCase().contains(query)
+	                	            		|| book.getCategory().toLowerCase().contains(query))
+	                	                System.out.println(book);
+	                	        }
+	                        	break;
+	                        case 2:
+	                        	System.out.print("Enter Book ID to borrow: ");
+	                	        int borrow_id = input.nextInt();
+	                	        for (Book book : Database.loadBooks()) {
+	                	            if (book.getId() == borrow_id && book.isAvailable()) {
+	                	                book.setAvailability(false);
+	                	                System.out.println("Book borrowed successfully.");
+	                	                break; // test
+	                	            }
+	                	        }
+	                	        System.out.println("Book unavailable.");
+	                        	break;
+	                        case 3: 
+	                        	System.out.print("Enter Book ID to return: ");
+	                	        int return_id = input.nextInt();
+	                	        for (Book book : Database.loadBooks()) {
+	                	            if (book.getId() == return_id && !book.isAvailable()) {
+	                	                book.setAvailability(true);
+	                	                System.out.println("Book returned successfully.");
+	                	                break; // test
+	                	            }
+	                	        }
+	                	        System.out.println("Invalid book ID.");
+	                        	break;
+	                        case 4: 
+	                        	for (Book book : Database.loadBooks()) {
+	                	            System.out.println(book);
+	                	        }
+	                        	break;
+	                        case 5:
+	                        	String password = input.nextLine();
+	                        	// TODO
+	                        	break;
+	                        case 6: 
+	                        	System.out.println("Exiting..."); 
+	                        default: 
+	                        	System.out.println("Invalid choice.");
+	                    }
+	                }
+	                else {
+	                    System.out.println("Invalid Student ID, access denied.");
+	                }
+	                break;
+            	case 2:
+	                Librarian librarian = new Librarian("admin123"); // Set password here
+	                librarian.showMenu();
+	                break;
+	           	default:
+	                System.out.println("Invalid choice, exiting..."); // TODO probably change to while loop
             }
             
             input.close();
