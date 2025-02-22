96772ef357a8005a4240c60321763fc12906485e
milesrtaylor <miles.taylor@student.nmt.edu>
2025-02-22 13:46:48 -0700

    added functions

--- project1/Transaction.java
+++ project1/Transaction.java
@@ -9,14 +9,16 @@ public class Transaction {
 	private LocalDate borrow_date;
 	private LocalDate due_date;
 	private LocalDate return_date;
+	private static int next_id;
 
-	public Transaction(int borrow_id, int student_id, int book_id, LocalDate borrow_date) {
+	// TODO might want two constructors (one with due date and one which does it automatically)
+	public Transaction(int borrow_id, int student_id, int book_id, LocalDate borrow_date, LocalDate due_date, LocalDate return_date) {
         this.borrow_id = borrow_id;
         this.student_id = student_id;
         this.book_id = book_id;
         this.borrow_date = borrow_date;
-        this.due_date = borrow_date.plusWeeks(2);
-        this.return_date = null;
+        this.due_date = due_date;
+        this.return_date = return_date;
     }
 
     public int getBorrowId() {
@@ -47,6 +49,17 @@ public class Transaction {
         return return_date == null && LocalDate.now().isAfter(due_date);
     }
 
+    public static void setNextId(int id) {
+        next_id = (id > 310) ? 301 : id; // Wrap around if needed
+    }
+    
+    public static int getNextBorrowId() {
+        if (next_id > 310) {
+            next_id = 301; // Wrap back to 301 if exceeding 310
+        }
+        return next_id++;
+    }
+    
     @Override
     public String toString() {
         return borrow_id + ", " + student_id + ", " + book_id + ", " + borrow_date + ", " + due_date + ", " + (return_date == null ? "Not Returned" : return_date);
