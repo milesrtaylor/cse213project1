bc8a56a8a7cf1aa7944c2bc4bc29851d9115485d
milesrtaylor <miles.taylor@student.nmt.edu>
2025-02-26 19:57:46 -0700

    all fixed

--- project1/MyMain.java
+++ project1/MyMain.java
@@ -24,114 +24,119 @@ public class MyMain {
 
             switch (choice) {
                 case 1:
-                    System.out.print("Enter your Student ID: ");
-                    int student_id = input.nextInt();
-                    input.nextLine();
-                    // TODO: might want to make sure input is an integer before moving on
                     Student currentStudent = null;
-                    for (Student student : students) {
-                        if (student.getId() == student_id) {
-                            currentStudent = student;
-                            break;
-                        }
-                    }
+                    try {
+                        System.out.print("Enter your Student ID: ");
+                        int student_id = Integer.parseInt(input.nextLine().trim());
 
-                    if (currentStudent != null) {
-                        System.out.println("\nStudent Library System:");
-                        while (true) { // Student menu loop
-                            System.out.println("1. Search Book");
-                            System.out.println("2. Borrow Book");
-                            System.out.println("3. Return Book");
-                            System.out.println("4. View All Books");
-                            System.out.println("5. Back to Main Menu");
-                            System.out.print("Choose an option: ");
-                            int student_choice = input.nextInt();
-                            input.nextLine();
-
-                            switch (student_choice) {
-                                case 1:
-                                    System.out.print("Enter title, author, or category: ");
-                                    String query = input.nextLine().toLowerCase();
-                                    for (Book book : books) {
-                                        if (book.getTitle().toLowerCase().contains(query)
-                                                || book.getAuthor().toLowerCase().contains(query)
-                                                || book.getCategory().toLowerCase().contains(query)) {
-                                            System.out.println(book);
-                                        }
-                                    }
-                                    break;
-                                case 2:
-                                    if (currentStudent.getBorrowedBooks().size() >= 3) {
-                                        System.out.println("You cannot borrow more than 3 books.");
-                                        break;
-                                    }
-                                    System.out.print("Enter Book ID to borrow: ");
-                                    int borrow_id = input.nextInt();
-                                    input.nextLine();
-                                    boolean borrowed = false;
-
-                                    for (Book book : books) {
-                                        if (book.getId() == borrow_id && book.isAvailable()) {
-                                            book.setAvailability(false);
-                                            int transaction_id = Transaction.getNextBorrowId();
-                                            LocalDate current_date = LocalDate.now();
-                                            LocalDate return_date = null;
-                                            transactions.add(new Transaction(transaction_id,
-                                            		currentStudent.getId(), book.getId(),current_date,
-                                            		current_date.plusWeeks(2), return_date));
-                                            currentStudent.getBorrowedBooks().add(book.getId());
-                                            currentStudent.borrowBook(book.getId());
-                                            System.out.println("Book borrowed successfully.");
-                                            borrowed = true;
-                                            break;
-                                        }
-                                    }
-
-                                    if (!borrowed) {
-                                        System.out.println("Book unavailable or ID invalid.");
-                                    }
-                                    break;
-                                case 3:
-                                    System.out.print("Enter Book ID to return: ");
-                                    int return_id = input.nextInt();
-                                    input.nextLine();
-                                    boolean returned = false;
-
-                                    for (Book book : books) {
-                                        if (book.getId() == return_id && !book.isAvailable()) {
-                                            book.setAvailability(true);
-                                            final Student studentRef = currentStudent; // Need a final reference for lambda expression
-                                            transactions.removeIf(t -> t.getBookId() == return_id
-                                            		&& t.getStudentId() == studentRef.getId());
-                                            currentStudent.returnBook(book.getId());
-                                            System.out.println("Book returned successfully.");
-                                            returned = true;
-                                            break;
-                                        }
-                                    }
-
-                                    if (!returned) {
-                                        System.out.println("Invalid book ID or book was not borrowed.");
-                                    }
-                                    break;
-                                case 4:
-                                    for (Book book : books) {
-                                        System.out.println(book);
-                                    }
-                                    break;
-                                case 5:
-                                    System.out.println("Returning to main menu...");
-                                    break;
-                                default:
-                                    System.out.println("Invalid choice.");
-                            }
-
-                            if (student_choice == 5) {
+                        for (Student student : students) {
+                            if (student.getId() == student_id) {
+                                currentStudent = student;
                                 break;
                             }
                         }
-                    } else {
-                        System.out.println("Invalid Student ID, access denied.");
+
+                        if (currentStudent == null) {
+                            System.out.println("Invalid Student ID, access denied.");
+                            break;
+                        }
+
+                    } catch (NumberFormatException e) {
+                        System.out.println("Invalid input. Please enter a numeric Student ID.");
+                        break;
+                    }
+
+                    System.out.println("\nStudent Library System:");
+                    while (true) { // Student menu loop
+                        System.out.println("1. Search Book");
+                        System.out.println("2. Borrow Book");
+                        System.out.println("3. Return Book");
+                        System.out.println("4. View All Books");
+                        System.out.println("5. Back to Main Menu");
+                        System.out.print("Choose an option: ");
+                        int student_choice = input.nextInt();
+                        input.nextLine();
+
+                        switch (student_choice) {
+                            case 1:
+                                System.out.print("Enter title, author, or category: ");
+                                String query = input.nextLine().toLowerCase();
+                                for (Book book : books) {
+                                    if (book.getTitle().toLowerCase().contains(query)
+                                            || book.getAuthor().toLowerCase().contains(query)
+                                            || book.getCategory().toLowerCase().contains(query)) {
+                                        System.out.println(book);
+                                    }
+                                }
+                                break;
+                            case 2:
+                                if (currentStudent.getBorrowedBooks().size() >= 3) {
+                                    System.out.println("You cannot borrow more than 3 books.");
+                                    break;
+                                }
+                                System.out.print("Enter Book ID to borrow: ");
+                                int borrow_id = input.nextInt();
+                                input.nextLine();
+                                boolean borrowed = false;
+                                for (Book book : books) {
+                                    if (book.getId() == borrow_id && book.isAvailable()) {
+                                        book.setAvailability(false);
+                                        int transaction_id = Transaction.getNextBorrowId();
+                                        LocalDate current_date = LocalDate.now();
+                                        LocalDate return_date = null;
+                                        transactions.add(new Transaction(transaction_id,
+                                        		currentStudent.getId(), book.getId(),current_date,
+                                        		current_date.plusWeeks(2), return_date));
+                                        currentStudent.getBorrowedBooks().add(book.getId());
+                                        currentStudent.borrowBook(book.getId());
+                                        System.out.println("Book borrowed successfully.");
+                                        borrowed = true;
+                                        break;
+                                    }
+                                }
+
+                                if (!borrowed) {
+                                    System.out.println("Book unavailable or ID invalid.");
+                                }
+                                break;
+                            case 3:
+                                System.out.print("Enter Book ID to return: ");
+                                int return_id = input.nextInt();
+                                input.nextLine();
+                                boolean returned = false;
+
+                                for (Book book : books) {
+                                    if (book.getId() == return_id && !book.isAvailable()) {
+                                        book.setAvailability(true);
+                                        final Student studentRef = currentStudent; // Need a final reference for lambda expression
+                                        transactions.removeIf(t -> t.getBookId() == return_id
+                                        		&& t.getStudentId() == studentRef.getId());
+                                        currentStudent.returnBook(book.getId());
+                                        System.out.println("Book returned successfully.");
+                                        returned = true;
+                                        break;
+                                    }
+                                }
+
+                                if (!returned) {
+                                    System.out.println("Invalid book ID or book was not borrowed.");
+                                }
+                                break;
+                            case 4:
+                                for (Book book : books) {
+                                    System.out.println(book);
+                                }
+                                break;
+                            case 5:
+                                System.out.println("Returning to main menu...");
+                                break;
+                            default:
+                                System.out.println("Invalid choice.");
+                        }
+                        
+                        if (student_choice == 5) {
+                            break;
+                        }
                     }
                     break;
 
@@ -142,7 +147,7 @@ public class MyMain {
                         return;
                     }
                     
-                    while (true) { // TODO: probably should change this to if loop to match student menu
+                    while (true) {
                         System.out.println("\nLibrarian Menu:");
                         System.out.println("1. List all books");
                         System.out.println("2. List borrowed books");
