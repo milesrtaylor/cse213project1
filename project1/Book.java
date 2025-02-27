f281877ff7790b2073988564119e150251559ce1
milesrtaylor <miles.taylor@student.nmt.edu>
2025-02-26 20:07:00 -0700

    LAST MINUTE MAJOR FIX

--- project1/Book.java
+++ project1/Book.java
@@ -25,16 +25,32 @@ public class Book {
     	return title;
     }
     
+    public void setTitle(String title) {
+    	this.title=title;
+    }
+    
     public String getAuthor() {
     	return author;
+    }
+    
+    public void setAuthor(String author) {
+    	this.author=author;
     }
     
     public String getIsbn() {
     	return isbn;
     }
     
+    public void setIsbn(String isbn) {
+    	this.isbn=isbn;
+    }
+    
     public String getCategory() {
     	return category;
+    }
+    
+    public void setCategory(String category) {
+    	this.category=category;
     }
     
     public boolean isAvailable() {
